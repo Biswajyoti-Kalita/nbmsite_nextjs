809d99321acff79675363b11c11247d2b3ed6500
Biswajyoti Kalita <biswajyotikalita@Biswajyotis-MacBook-Air.local>
2026-04-09 22:04:12 +0530

    updated GTAG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -119,7 +119,7 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WDD485HL');`
+})(window,document,'script','dataLayer','GTM-58DTSWTK');`
         }} />
         {/* End Google Tag Manager */}
       </head>
@@ -129,7 +129,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WDD485HL"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-58DTSWTK"
             height="0" 
             width="0" 
             style={{display:'none',visibility:'hidden'}}
